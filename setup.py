46dd49271a3daa92c0c93f483a391d1240b4fe68
Richard Hartmann <cimatosa@gmx.de>
2017-09-28 16:20:02 +0200

    setup.py checks for c lib dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 from setuptools.extension import Extension
-
+from ctypes.util import find_library
 from Cython.Build import cythonize
 
 author      = "Richard Hartmann"
@@ -8,6 +8,24 @@ authors     = [author]
 description = 'special functions with complex arguments - wrapped for python'
 name        = 'cplxfnc'
 version     = '0.1'
+
+def check_for_lib(*names):
+    lib_found = None
+    for n in names:
+        l = find_library(n)
+        if l is not None:
+            lib_found = n
+            print("found c library {}".format(l))
+            break
+
+    if lib_found is None:
+        raise RuntimeError("shared library '{}' not found".format(name))
+    return lib_found
+
+libraries = []
+libraries.append(check_for_lib('m'))
+libraries.append(check_for_lib('flint'))
+libraries.append(check_for_lib('arb', 'flint-arb'))
 
 setup(
       name=name,
@@ -21,7 +39,7 @@ setup(
       long_description=description,
       ext_modules = cythonize(Extension("cplxfnc.cplxfnc_cyth",
                                        ["./cplxfnc/cplxfnc_cyth.pyx", "./cplxfnc_clib/cplxfnc.cpp"],
-                                       libraries = ['m', 'flint', 'arb'],
+                                       libraries = libraries,
                                        extra_compile_args=['-std=c++11'],
                                        language="c++"))
 )
